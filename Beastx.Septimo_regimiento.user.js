519e29dbb4e7d8d20cd4f5ddee5deb5ec90ce608
Leandro Asrilevich <beastx@gmail.com>
2009-11-05 04:12:27 -0300

    added new requires.. some fixes for tinkerman use.. added exclude page values

--- Beastx.Septimo_regimiento.user.js
+++ Beastx.Septimo_regimiento.user.js
@@ -2,7 +2,10 @@
 // @name                  Septimo regimiento
 // @namespace       Beastx
 // @description        Scripts para la alianza 7mo Regimiento
-// @include               http://*.ikariam.com/*
+
+// @include               http://s*.ikariam.*/*
+// @exclude              http://support.ikariam.*
+// @exclude              http://board.*.ikariam/*
 
 // @require               http://ikariam.beastx/tools/userScripts/ikariam/IkaTools.js
 
@@ -17,15 +20,16 @@
 // @require               http://ikariam.beastx/tools/userScripts/requires/FloatingPopup.js
 // @require               http://ikariam.beastx/tools/userScripts/requires/TabManager.js
 
+// @require               http://ikariam.beastx/tools/userScripts/ikariam/CitiesList.js
 // @require               http://ikariam.beastx/tools/userScripts/ikariam/City.js
 // @require               http://ikariam.beastx/tools/userScripts/ikariam/Building.js
+// @require               http://ikariam.beastx/tools/userScripts/ikariam/BuildingsList.js
 // @require               http://ikariam.beastx/tools/userScripts/ikariam/Island.js
 // @require               http://ikariam.beastx/tools/userScripts/ikariam/Player.js
 // @require               http://ikariam.beastx/tools/userScripts/ikariam/Alliance.js
 // @require               http://ikariam.beastx/tools/userScripts/ikariam/Resource.js
 
 // @require               http://ikariam.beastx/tools/userScripts/modules/AllianceMap.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/AllianceViewImprover.js
 // @require               http://ikariam.beastx/tools/userScripts/modules/AlliedOnlineAdvisor.js
 // @require               http://ikariam.beastx/tools/userScripts/modules/CombatConverter.js
 // @require               http://ikariam.beastx/tools/userScripts/modules/DataSaver.js
@@ -40,6 +44,7 @@
 // @require               http://ikariam.beastx/tools/userScripts/modules/MessageManager.js
 // @require               http://ikariam.beastx/tools/userScripts/modules/OurDeployedCitiesCleaner.js
 // @require               http://ikariam.beastx/tools/userScripts/modules/PillageHelper.js
+// @require               http://ikariam.beastx/tools/userScripts/modules/ResearchAdvisorHelper.js
 // @require               http://ikariam.beastx/tools/userScripts/modules/ScrollbarRemover.js
 // @require               http://ikariam.beastx/tools/userScripts/modules/ShipmentTimes.js
 // @require               http://ikariam.beastx/tools/userScripts/modules/ShipMovementsViewImprover.js
@@ -48,6 +53,10 @@
 // @require               http://ikariam.beastx/tools/userScripts/modules/TriumphalArch.js
 // @require               http://ikariam.beastx/tools/userScripts/modules/UnitFormatter.js
 // @require               http://ikariam.beastx/tools/userScripts/modules/UpgradeWatcher.js
+// @require               http://ikariam.beastx/tools/userScripts/modules/ResearchPointsHelper.js
+// @require               http://ikariam.beastx/tools/userScripts/modules/GroupMessenger.js
+// @require               http://ikariam.beastx/tools/userScripts/modules/ArribalTimesImprover.js
+// @require               http://ikariam.beastx/tools/userScripts/modules/CombatLayoutHelper.js
 
 // @require               http://ikariam.beastx/tools/userScripts/TopMenu.js
 // @require               http://ikariam.beastx/tools/userScripts/OptionsPage.js
@@ -77,10 +86,13 @@ function instanceLog() {
         unsafeWindow.LOG.logRunner = new unsafeWindow.LOG.LogRunner(document);
         unsafeWindow.LOG.loaded = true;
         unsafeWindow.LOG.setTypeName(IkariamTools, 'ikatools');
+        unsafeWindow.LOG.setTypeName(Beastx.AllianceObject, 'Beastx.AllianceObject');
         unsafeWindow.LOG.setTypeName(Beastx.ResourceObject, 'Beastx.ResourceObject');
         unsafeWindow.LOG.setTypeName(Beastx.BuildingObject, 'Beastx.BuildingObject');
         unsafeWindow.LOG.setTypeName(Beastx.PlayerObject, 'Beastx.PlayerObject');
         unsafeWindow.LOG.setTypeName(Beastx.CityObject, 'Beastx.CityObject');
+        unsafeWindow.LOG.setTypeName(Beastx.CitiesList, 'Beastx.CitiesList');
+        unsafeWindow.LOG.setTypeName(Beastx.BuildingsList, 'Beastx.BuildingsList');
     } else {
         setTimeout(instanceLog);
     }
@@ -88,71 +100,76 @@ function instanceLog() {
 
 unsafeWindow.onunload = function() {
     GM_setValue('logAdded', false);
+    GM_setValue('logAdded2', false);
 }
 
-Beastx.init('0.2');
+if (!GM_getValue('logAdded2')) {
+    GM_setValue('logAdded2', true);
+    Beastx.init('0.2');
+    
+    IkaTools = New(IkariamTools);
+    IkaTools.loadData();
 
-IkaTools = New(IkariamTools);
+    var actualView = IkaTools.getView();
 
-var actualView = IkaTools.getView();
+    if (actualView == 'login') {
+        $('universe').value = 's2.ar.ikariam.com';
+    } else {
+        
+        if (!Beastx.getGMValue('firstRun')) {
+            Beastx.setGMValue('firstRun', true);
+            location.href = "http://" + IkaTools.getDomain() + "/index.php?view=options";
+        }
+        
+        if (Beastx.Config.options.showMenu) {
+            var mainMenu = New(Beastx.TopMenu);
+        }
+        
+        if (Beastx.Config.options.thinView) {
+            var thinViews = New(Beastx.ThinViews, [ actualView ]);
+        }
+        if (Beastx.Config.options.scrollBarRemover) {
+            var scrollBarRemover = New(Beastx.ScrollBarRemover);
+        }
+        
+        if (Beastx.Config.options.dataSaver) {
+            var dataSaver = New(Beastx.DataSaver, [ Beastx.Config.postUrl, actualView ]);
+        }
+        
+        if (actualView == 'options') {
+            var optionsPage = New(Beastx.OptionsPage);
+        }
+        
+        if (actualView == 'embassyGeneralAttacksFromAlly' && Beastx.Config.options.militaryPageImprover) {
+            New(Beastx.GeneralViewPageCleaner);
+        }
+        
+        if (Beastx.Config.options.ourDeployedCitiesCleaner) {
+            New(Beastx.OurDeployedCitiesCleaner);
+        }
+        
+        if (Beastx.Config.options.transportResourcesHelper) {
+            New(Beastx.TransportResourcesHelper, [ actualView ]);
+        }
+        
+        if (Beastx.Config.options.shipMovementsViewImprover) {
+            New(Beastx.ShipMovementsViewImprover, [ actualView ]);
+        }
+        
+        if (Beastx.Config.options.alliedOnlineAdvisor) {
+            New(Beastx.AlliedOnlineAdvisor, [ actualView ]);
+        }
+        
+        if (Beastx.Config.options.upgradeWatcher) {
+            New(Beastx.UpgradeWatcher, [ actualView ]);
+        }
+        
+        if (Beastx.Config.options.inlineScore) {
+            New(Beastx.InlineScore, [ actualView ]);
+        }
+        
+    };
 
-if (actualView == 'login') {
-    $('universe').value = 's2.ar.ikariam.com';
-} else {
-    
-    if (!Beastx.getGMValue('firstRun')) {
-        Beastx.setGMValue('firstRun', true);
-        location.href = "http://" + IkaTools.getDomain() + "/index.php?view=options";
-    }
-    
-    if (Beastx.Config.options.showMenu) {
-        var mainMenu = New(Beastx.TopMenu);
-    }
-    
-    if (Beastx.Config.options.thinView) {
-        var thinViews = New(Beastx.ThinViews, [ actualView ]);
-    }
-    if (Beastx.Config.options.scrollBarRemover) {
-        var scrollBarRemover = New(Beastx.ScrollBarRemover);
-    }
-    
-    if (Beastx.Config.options.dataSaver) {
-        var dataSaver = New(Beastx.DataSaver, [ Beastx.Config.postUrl, actualView ]);
-    }
-    
-    if (actualView == 'options') {
-        var optionsPage = New(Beastx.OptionsPage);
-    }
-    
-    if (actualView == 'embassyGeneralAttacksFromAlly' && Beastx.Config.options.militaryPageImprover) {
-        New(Beastx.GeneralViewPageCleaner);
-    }
-    
-    if (Beastx.Config.options.ourDeployedCitiesCleaner) {
-        New(Beastx.OurDeployedCitiesCleaner);
-    }
-    
-    if (Beastx.Config.options.transportResourcesHelper) {
-        New(Beastx.TransportResourcesHelper, [ actualView ]);
-    }
-    
-    if (Beastx.Config.options.shipMovementsViewImprover) {
-        New(Beastx.ShipMovementsViewImprover, [ actualView ]);
-    }
-    
-    if (Beastx.Config.options.alliedOnlineAdvisor) {
-        New(Beastx.AlliedOnlineAdvisor, [ actualView ]);
-    }
-    
-    if (Beastx.Config.options.upgradeWatcher) {
-        New(Beastx.UpgradeWatcher, [ actualView ]);
-    }
-    
-    if (Beastx.Config.options.inlineScore) {
-        New(Beastx.InlineScore, [ actualView ]);
-    }
-    
-};
-
-var scriptUpdater = New(ScriptUpdater, [ 'Beastx.Septimo_regimiento.user.js', '0.3', true ]);
-scriptUpdater.check();
+    var scriptUpdater = New(ScriptUpdater, [ 'Beastx.Septimo_regimiento.user.js', '0.3', true ]);
+    scriptUpdater.check();
+}
