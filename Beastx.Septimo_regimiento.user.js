4cee61595dbed336f369d1706e4ff8c5df742a4b
Leandro Asrilevich <beastx@gmail.com>
2009-11-16 06:20:25 -0300

    several changes

--- Beastx.Septimo_regimiento.user.js
+++ Beastx.Septimo_regimiento.user.js
@@ -9,63 +9,68 @@
 // @exclude              http://*.ikariam.*/*?view=premiumPayment
 // @exclude              http://*.ikariam.*/*?view=premium
 
-// @require               http://ikariam.beastx/tools/userScripts/ikariam/IkaTools.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/ikariam/IkaTools.js
 
-// @require               http://ikariam.beastx/tools/userScripts/requires/Json.js
-// @require               http://ikariam.beastx/tools/userScripts/requires/VAR.js
-// @require               http://ikariam.beastx/tools/userScripts/requires/DOM.js
-// @require               http://ikariam.beastx/tools/userScripts/requires/Beastx.js
-// @require               http://ikariam.beastx/tools/userScripts/requires/ScriptUpdater.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/requires/Json.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/requires/VAR.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/requires/DOM.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/requires/Beastx.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/requires/ScriptUpdater.js
 
-// @require               http://ikariam.beastx/tools/userScripts/requires/ColorSelector.js
-// @require               http://ikariam.beastx/tools/userScripts/requires/DragManager.js
-// @require               http://ikariam.beastx/tools/userScripts/requires/FloatingPopup.js
-// @require               http://ikariam.beastx/tools/userScripts/requires/TabManager.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/requires/ColorSelector.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/requires/DragManager.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/requires/FloatingPopup.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/requires/TabManager.js
 
-// @require               http://ikariam.beastx/tools/userScripts/ikariam/CitiesList.js
-// @require               http://ikariam.beastx/tools/userScripts/ikariam/City.js
-// @require               http://ikariam.beastx/tools/userScripts/ikariam/Building.js
-// @require               http://ikariam.beastx/tools/userScripts/ikariam/BuildingsList.js
-// @require               http://ikariam.beastx/tools/userScripts/ikariam/Island.js
-// @require               http://ikariam.beastx/tools/userScripts/ikariam/Player.js
-// @require               http://ikariam.beastx/tools/userScripts/ikariam/Alliance.js
-// @require               http://ikariam.beastx/tools/userScripts/ikariam/Resource.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/ikariam/CitiesList.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/ikariam/City.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/ikariam/Building.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/ikariam/BuildingsList.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/ikariam/Island.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/ikariam/Player.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/ikariam/Alliance.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/ikariam/Resource.js
 
-// @require               http://ikariam.beastx/tools/userScripts/modules/AllianceMap.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/AlliedOnlineAdvisor.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/CombatConverter.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/DataSaver.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/DistanceCalculator.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/DonationsPageHelper.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/Embassytools.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/EmpireBoard.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/FavoriteTargets.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/GeneralViewPageCleaner.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/InlineScore.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/LibraryImprovements.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/MessageManager.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/OurDeployedCitiesCleaner.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/PillageHelper.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/ResearchAdvisorHelper.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/ScrollbarRemover.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/ShipmentTimes.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/ShipMovementsViewImprover.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/ThinViews.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/TransportResourcesHelper.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/TriumphalArch.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/UnitFormatter.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/UpgradeWatcher.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/ResearchPointsHelper.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/GroupMessenger.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/ArribalTimesImprover.js
-// @require               http://ikariam.beastx/tools/userScripts/modules/CombatLayoutHelper.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/AllianceMap.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/AlliedOnlineAdvisor.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/CombatConverter.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/DataSaver.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/DistanceCalculator.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/DonationsPageHelper.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/Embassytools.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/EmpireBoard.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/FavoriteTargets.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/GeneralViewPageCleaner.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/InlineScore.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/LibraryImprovements.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/MessageManager.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/OurDeployedCitiesCleaner.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/PillageHelper.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/ResearchAdvisorHelper.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/ScrollbarRemover.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/ShipmentTimes.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/ShipMovementsViewImprover.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/ThinViews.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/TransportResourcesHelper.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/TriumphalArch.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/UnitFormatter.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/UpgradeWatcher.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/ResearchPointsHelper.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/GroupMessenger.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/ArribalTimesImprover.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/modules/CombatLayoutHelper.js
 
-// @require               http://ikariam.beastx/tools/userScripts/TopMenu.js
-// @require               http://ikariam.beastx/tools/userScripts/OptionsPage.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/TopMenu.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/OptionsPage.js
+// @require               http://7moreg.beastx.com.ar/tools/userScripts/Notes.js
 
-// @version               0.3
+// @version               0.6
 // @author                Beastx
 //
+// @history                0.6 added donation page helper module
+// @history                0.6 added arribal time module
+// @history                0.5 fixed problem width main menu
+// @history                0.4 several changes in ikatools
 // @history                0.3 reorganice all requirements into folders
 // @history                0.3 Fixed update message when user run the script the first time and have the newest version already
 // @history                0.3 Go to options page direclty when script is installed
@@ -76,11 +81,11 @@
 // ==/UserScript==
 
 if (!GM_getValue('logAdded')) {
-    GM_setValue('logAdded', true);
-    unsafeWindow.LOG = document.createElement('script');
-    unsafeWindow.LOG.setAttribute('src','http://ikariam.beastx/tools/log/log.js');
-    document.body.appendChild(unsafeWindow.LOG);
-    setTimeout(instanceLog);
+    //~ GM_setValue('logAdded', true);
+    //~ unsafeWindow.LOG = document.createElement('script');
+    //~ unsafeWindow.LOG.setAttribute('src','http://ikariam.beastx/tools/log/log.js');
+    //~ document.body.appendChild(unsafeWindow.LOG);
+    //~ setTimeout(instanceLog);
 }
 
 function instanceLog() {
@@ -95,6 +100,14 @@ function instanceLog() {
         unsafeWindow.LOG.setTypeName(Beastx.CityObject, 'Beastx.CityObject');
         unsafeWindow.LOG.setTypeName(Beastx.CitiesList, 'Beastx.CitiesList');
         unsafeWindow.LOG.setTypeName(Beastx.BuildingsList, 'Beastx.BuildingsList');
+        unsafeWindow.LOG.setTypeName(FloatingPopup, 'FloatingPopup');
+        unsafeWindow.LOG.setTypeName(FloatingMenu, 'FloatingMenu');
+        unsafeWindow.LOG.setTypeName(FloatingMenuItem, 'FloatingMenuItem');
+        unsafeWindow.LOG.setTypeName(Beastx.TopMenu, 'Beastx.TopMenu');
+        unsafeWindow.LOG.setTypeName(Beastx.TopMenuButton, 'Beastx.TopMenuButton');
+        unsafeWindow.LOG.setTypeName(Beastx.Notes, 'Beastx.Notes');
+        unsafeWindow.LOG.setTypeName(Beastx.OurDeployedCitiesCleaner, 'Beastx.OurDeployedCitiesCleaner');
+        unsafeWindow.LOG.setTypeName(Beastx.OurDeployedCitiesManager, 'Beastx.OurDeployedCitiesManager');
     } else {
         setTimeout(instanceLog);
     }
@@ -107,79 +120,39 @@ unsafeWindow.onunload = function() {
 
 if (!GM_getValue('logAdded2')) {
     GM_setValue('logAdded2', true);
-    Beastx.init('0.2');
     
     IkaTools = New(IkariamTools);
     IkaTools.loadData();
 
+    Beastx.myNotes = NewModule(Beastx.Notes);
     var actualView = IkaTools.getView();
-
+    var mainMenu = NewModule(Beastx.TopMenu);
+    
     if (actualView == 'login') {
         $('universe').value = 's2.ar.ikariam.com';
     } else {
         
-        if (!Beastx.getGMValue('firstRun')) {
-            Beastx.setGMValue('firstRun', true);
-            location.href = "http://" + IkaTools.getDomain() + "/index.php?view=options";
-        }
-        
-        if (Beastx.Config.options.showMenu) {
-            var mainMenu = New(Beastx.TopMenu);
-        }
-        
-        if (Beastx.Config.options.thinView) {
-            var thinViews = New(Beastx.ThinViews, [ actualView ]);
-        }
-        if (Beastx.Config.options.scrollBarRemover) {
-            var scrollBarRemover = New(Beastx.ScrollBarRemover);
-        }
-        
-        if (Beastx.Config.options.dataSaver) {
-            var dataSaver = New(Beastx.DataSaver, [ Beastx.Config.postUrl, actualView ]);
-        }
+        //~ var dataSaver = New(Beastx.DataSaver, [ Beastx.Config.postUrl, actualView ]);
         
         if (actualView == 'options') {
-            var optionsPage = New(Beastx.OptionsPage);
+            NewModule(Beastx.OptionsPage);
         }
         
-        if (actualView == 'embassyGeneralAttacksFromAlly' && Beastx.Config.options.militaryPageImprover) {
-            New(Beastx.GeneralViewPageCleaner);
-        }
-        
-        if (Beastx.Config.options.ourDeployedCitiesCleaner) {
-            New(Beastx.OurDeployedCitiesCleaner);
-        }
-        
-        if (Beastx.Config.options.transportResourcesHelper) {
-            New(Beastx.TransportResourcesHelper, [ actualView ]);
-        }
-        
-        if (Beastx.Config.options.shipMovementsViewImprover) {
-            New(Beastx.ShipMovementsViewImprover, [ actualView ]);
-        }
-        
-        if (Beastx.Config.options.alliedOnlineAdvisor) {
-            New(Beastx.AlliedOnlineAdvisor, [ actualView ]);
-        }
-        
-        if (Beastx.Config.options.upgradeWatcher) {
-            New(Beastx.UpgradeWatcher, [ actualView ]);
-        }
-        
-        if (Beastx.Config.options.inlineScore) {
-            New(Beastx.InlineScore, [ actualView ]);
-        }
-        
-        if (Beastx.Config.options.empireBoard) {
-            New(Beastx.EmpireBoard, [ actualView ]);
-        }
-        
-        if (Beastx.Config.options.embassyTools) {
-            New(Beastx.EmbassyTools, [ actualView ]);
-        }
-        
+        if (Beastx.Config.options.OurDeployedCitiesCleaner && Beastx.Config.options.OurDeployedCitiesCleaner.enabled) { NewModule(Beastx.OurDeployedCitiesCleaner); }
+        if (Beastx.Config.options.ThinViews && Beastx.Config.options.ThinViews.enabled) { var thinViews = NewModule(Beastx.ThinViews, [ actualView ]); }
+        if (Beastx.Config.options.ScrollbarRemover && Beastx.Config.options.ScrollbarRemover.enabled) { var scrollbarRemover = NewModule(Beastx.ScrollbarRemover); }
+        if (Beastx.Config.options.GeneralViewPageCleaner && Beastx.Config.options.GeneralViewPageCleaner.enabled) { NewModule(Beastx.GeneralViewPageCleaner); }
+        if (Beastx.Config.options.TransportResourcesHelper && Beastx.Config.options.TransportResourcesHelper.enabled) { NewModule(Beastx.TransportResourcesHelper, [ actualView ]); }
+        if (Beastx.Config.options.ArribalTimesImprover && Beastx.Config.options.ArribalTimesImprover.enabled) { NewModule(Beastx.ArribalTimesImprover, [ actualView ]); }
+        if (Beastx.Config.options.ShipMovementsViewImprover && Beastx.Config.options.ShipMovementsViewImprover.enabled) { NewModule(Beastx.ShipMovementsViewImprover, [ actualView ]); }
+        if (Beastx.Config.options.AlliedOnlineAdvisor && Beastx.Config.options.AlliedOnlineAdvisor.enabled) { NewModule(Beastx.AlliedOnlineAdvisor, [ actualView ]); }
+        if (Beastx.Config.options.UpgradeWatcher && Beastx.Config.options.UpgradeWatcher.enabled) { NewModule(Beastx.UpgradeWatcher, [ actualView ]); }
+        if (Beastx.Config.options.InlineScore && Beastx.Config.options.InlineScore.enabled) { NewModule(Beastx.InlineScore, [ actualView ]); }
+        if (Beastx.Config.options.EmpireBoard && Beastx.Config.options.EmpireBoard.enabled) { NewModule(Beastx.EmpireBoard, [ actualView ]); }
+        if (Beastx.Config.options.EmbassyTools && Beastx.Config.options.EmbassyTools.enabled) { NewModule(Beastx.EmbassyTools, [ actualView ]); }
+        if (Beastx.Config.options.DonationsPageHelper && Beastx.Config.options.DonationsPageHelper.enabled) { NewModule(Beastx.DonationsPageHelper, [ actualView ]); }
     };
 
-    var scriptUpdater = New(ScriptUpdater, [ 'Beastx.Septimo_regimiento.user.js', '0.3', true ]);
-    scriptUpdater.check();
+    //~ var scriptUpdater = New(ScriptUpdater, [ 'Beastx.Septimo_regimiento.user.js', '0.6', true ]);
+    //~ scriptUpdater.check();
 }
